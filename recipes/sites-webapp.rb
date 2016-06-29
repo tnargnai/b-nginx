f6ed0966d464a71926381c80d4c7e6ff47cc99e4
ian.grant <tnargnai@gmail.com>
2016-06-29 16:50:02 +0100

    Added vars to template

--- recipes/sites-webapp.rb
+++ recipes/sites-webapp.rb
@@ -5,11 +5,14 @@
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 
 # Site Admin
-template "/etc/nginx/sites-available/#{node['b-nginx']['site']['name']}" do
+template "/etc/nginx/sites-available/#{node['b-nginx']['site']['domain']}" do
   source node['b-nginx']['site']['config']
   mode 0644
   owner 'root'
   group 'root'
+  variables(
+      :domain_name => node['b-nginx']['domain']
+  )
 end
 
 nginx_site node['b-nginx']['site']['name'] do
