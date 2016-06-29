3546d646ed8662ffaa4948e9e0f8bb628c2d41a5
ian.grant <tnargnai@gmail.com>
2016-06-29 16:54:14 +0100

    removed atttibute values

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,4 @@
 default['nginx']['default_site_enabled'] = false
 
-default['b-nginx']['site']['domain'] = 'webapp.sb.com'
-default['b-nginx']['site']['config'] = 'etc/nginx/sites-available/webapp.sb.com.erb'
+default['b-nginx']['site']['domain'] = '' # webapp.sb.com
+default['b-nginx']['site']['config'] = "etc/nginx/sites-available/#{node['b-nginx']['site']['name']}"
