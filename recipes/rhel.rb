9c318564a39e9b9a97b5ba40cbe63a7c93d7e66e
ian.grant <tnargnai@gmail.com>
2016-07-28 15:34:05 +0100

    removed config so it just installs nginx

--- recipes/rhel.rb
+++ recipes/rhel.rb
@@ -16,23 +16,3 @@ end
 package 'nginx' do
   timeout 60
 end
-
-# Set service
-service "nginx" do
-  supports :status => true, :restart => true, :reload => true
-  action [ :enable ]
-end
-
-# Site Admin
-template "/etc/nginx/conf.d/default.conf" do
-  source 'etc/nginx/conf.d/default.conf.erb'
-  mode 0644
-  owner 'root'
-  group 'root'
-  variables(
-      :app_servers => node['b-nginx']['site']['app_servers'],
-      :backend_port => node['b-nginx']['site']['backend_port'],
-      :domain_name => node['b-nginx']['site']['domain']
-  )
-  notifies :restart, "service[nginx]"
-end
