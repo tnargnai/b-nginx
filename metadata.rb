92717578a7deb13a497412b58a73997fa2dba41a
ian.grant <tnargnai@gmail.com>
2016-08-12 13:39:04 +0100

    stuff

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'ian.grant@barclays.com'
 license 'all_rights'
 description 'Installs/Configures b-nginx'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.1.1'
+version '0.1.2'
 
 supports 'debian'
 supports 'ubuntu'
