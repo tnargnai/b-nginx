b806a76f9175a8b2a5c429365d24bf3d03c103d7
ian.grant <tnargnai@gmail.com>
2016-08-12 16:09:28 +0100

    Removed config

--- recipes/deb.rb
+++ recipes/deb.rb
@@ -7,19 +7,19 @@
 include_recipe 'apt'
 include_recipe 'nginx'
 
-# Site Admin
-template "/etc/nginx/sites-available/#{node['b-nginx']['site']['domain']}" do
-  source 'etc/nginx/sites-available/site.conf.erb'
-  mode 0644
-  owner 'root'
-  group 'root'
-  variables(
-  :app_servers => node['b-nginx']['site']['app_servers'],
-  :backend_port => node['b-nginx']['site']['backend_port'],
-  :domain_name => node['b-nginx']['site']['domain']
-  )
-end
-
-nginx_site node['b-nginx']['site']['domain'] do
-  action :enable
-end
+# # Site Admin
+# template "/etc/nginx/sites-available/#{node['b-nginx']['site']['domain']}" do
+#   source 'etc/nginx/sites-available/site.conf.erb'
+#   mode 0644
+#   owner 'root'
+#   group 'root'
+#   variables(
+#   :app_servers => node['b-nginx']['site']['app_servers'],
+#   :backend_port => node['b-nginx']['site']['backend_port'],
+#   :domain_name => node['b-nginx']['site']['domain']
+#   )
+# end
+#
+# nginx_site node['b-nginx']['site']['domain'] do
+#   action :enable
+# end
